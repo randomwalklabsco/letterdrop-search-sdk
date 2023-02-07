6be78c8d065ae7e3d6b86977ba04e30e06a4987d
vinodkumar2019 <52480871+vinodkumar2019@users.noreply.github.com>
2023-02-07 10:06:16 +0530

    Added made with letterdrop

--- utils/sdk.js
+++ utils/sdk.js
@@ -1,39 +1,112 @@
-(function () {
-  const BASE_URL = "https://transcript-analysis-3k44zkbrua-uc.a.run.app";
-  async function getSearchResult(question, publicationId) {
-    try {
-      const formData = new FormData();
-      formData.set("question", question);
-      formData.set("publicationId", publicationId);
+const input = document.getElementById("letterdrop-input");
+const results = document.getElementById("letterdrop-results");
+const examples = [
+  document.getElementById("letterdrop-example1"),
+  document.getElementById("letterdrop-example2")
+];
+const loader = document.getElementById("letterdrop-loader");
+const readMore = document.getElementById("letterdrop-read-more");
+var loading = false;
 
-      const response = await fetch(`${BASE_URL}/querycontent`, {
+examples.forEach((example) => {
+  example.addEventListener("click", async () => {
+    if (loading) return;
+    input.value = example.value;
+    getAnswer();
+  });
+});
+
+input.addEventListener("keydown", async (event) => {
+  if (event.key === "Enter") getAnswer();
+});
+
+function toggleLoading() {
+  loading = !loading;
+  loader.style.display = loading ? "block" : "none";
+  results.innerText = loading ? "" : results.innerText;
+  document.getElementById("letterdrop-example-search").style.display = "none";
+  readMore.style.display = loading ? "none" : "flex";
+}
+
+async function getAnswer() {
+  if (loading || !input.value?.trim()) return;
+
+  toggleLoading();
+  try {
+    const formData = new FormData();
+    formData.set("question", input.value);
+    formData.set("publicationId", publicationId);
+
+    const response = await fetch(
+      "https://cc24-157-119-208-198.in.ngrok.io/querycontent",
+      {
         method: "POST",
         body: formData
-      });
-
-      const {
-        success,
-        answer = "We were unable to answer your query. Try again.",
-        source = []
-      } = await response.json();
-
-      if (success) {
-        return {
-          answer,
-          source
-        };
       }
-    } catch (error) {
-      console.error("Error: ", error);
-      results.innerText = "We were unable to answer your query. Try again.";
-    }
-    return {
-      answer: "We were unable to answer your query. Try again.",
-      source: []
-    };
-  }
+    );
+    const {
+      success,
+      answer = "We were unable to answer your query. Try again.",
+      source = []
+    } = await response.json();
 
-  module.exports = {
-    getSearchResult
-  };
-})();
+    results.innerText = success
+      ? answer
+      : "We were unable to answer your query. Try again.";
+    if (success) {
+      addReadMoreArticles(source);
+    }
+  } catch (error) {
+    console.error("Error: ", error);
+    results.innerText = "We were unable to answer your query. Try again.";
+  }
+  toggleLoading();
+}
+
+function addReadMoreArticles(sourceList) {
+  sourceList = [...sourceList, ...sourceList, ...sourceList];
+  if (sourceList?.length) {
+    readMore.style.display = "flex";
+    readMore.innerHTML = "";
+    readMore.innerHTML = "<label>Read more</label>";
+    sourceList.forEach((source) => {
+      let { url, image, title, description } = source;
+
+      const articleDiv = document.createElement("div");
+      articleDiv.className = "letterdrop-article";
+
+      const imageElement = document.createElement("img");
+      imageElement.src = image;
+      imageElement.alt = "Cover image";
+      imageElement.className = "letterdrop-image";
+
+      const articleDetails = document.createElement("div");
+      articleDetails.className = "letterdrop-article-details";
+
+      const titleElement = document.createElement("label");
+      titleElement.innerText = title;
+      titleElement.className = "letterdrop-article-title letterdrop-ellipsis";
+
+      const descriptionElement = document.createElement("label");
+      descriptionElement.innerText = description;
+      descriptionElement.className =
+        "letterdrop-article-description letterdrop-ellipsis";
+
+      const anchorElement = document.createElement("a");
+      anchorElement.href = url;
+      anchorElement.innerText = url;
+      anchorElement.target = "_blank";
+      anchorElement.rel = "noreferrer";
+      anchorElement.className = "letterdrop-article-link letterdrop-ellipsis";
+
+      articleDetails.appendChild(titleElement);
+      articleDetails.appendChild(descriptionElement);
+      articleDetails.appendChild(anchorElement);
+
+      articleDiv.appendChild(imageElement);
+      articleDiv.appendChild(articleDetails);
+
+      readMore.appendChild(articleDiv);
+    });
+  }
+}
